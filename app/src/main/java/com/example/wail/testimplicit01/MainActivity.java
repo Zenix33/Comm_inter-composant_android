d8b2e945f3911af769ea85d11555bf7134482142
Zenix42 <wlaiouar@gmail.com>
2017-10-17 21:33:01 +0200

    espace en plus

--- app/src/main/java/com/example/wail/testimplicit01/MainActivity.java
+++ app/src/main/java/com/example/wail/testimplicit01/MainActivity.java
@@ -37,23 +37,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
 
             case R.id.button1:
-                Log.i("MyApp", "APPEL");
+                Log.i("MyApp", " APPEL");
                 break;
             case R.id.button2:
-                Log.i("MyApp2", "MMS");
+                Log.i("MyApp2", " MMS");
                 break;
             case R.id.button3:
-                Log.i("MyApp3", "SMS");
+                Log.i("MyApp3", " SMS");
                 break;
             case R.id.button4:
-                Log.i("MyApp4", "WEB");
+                Log.i("MyApp4", " WEB");
                 break;
             case R.id.button5:
-                Log.i("MyApp5", "MAP");
+                Log.i("MyApp5", " MAP");
                 break;
         }
     }
-    ;
+    
 }
 
 
